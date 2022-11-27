199b3a8068817aa23c3db49ec6912ea4710b50f2
dzhelev <dzhelev@vmware.com>
2022-11-27 18:03:47 +0200

    fix cicd

--- root/docker-entrypoint.sh
+++ root/docker-entrypoint.sh
@@ -11,13 +11,13 @@ die()
 rm -f /var/log/monit.log
 ln -s /proc/1/fd/1 /var/log/monit.log
 
-### Check monit conf
-test -e /config/monitrc || die "Monit conf not found at: /config/monitrc"
-/usr/local/bin/monit -c /config/monitrc -t || die "Monit conf verification failed"
-
 ### Set perms
 chown root:root /config/monitrc
 chmod 600 /config/monitrc
+
+### Check monit conf
+test -e /config/monitrc || die "Monit conf not found at: /config/monitrc"
+/usr/local/bin/monit -c /config/monitrc -t || die "Monit conf verification failed"
 
 ### Run
 /usr/local/bin/monit -c /config/monitrc -I
