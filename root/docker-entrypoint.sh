c971db0fa45a462b5b7dacc9d45a569c41a6d203
Daniel <daniel@localhost.org>
2024-04-11 18:54:14 +0300

    add

--- root/docker-entrypoint.sh
+++ root/docker-entrypoint.sh
@@ -7,14 +7,6 @@ die()
  exit 1
 }
 
-### Setup logging
-rm -f /var/log/monit.log
-ln -s /proc/1/fd/1 /var/log/monit.log
-
-### Set perms
-chown root:root /config/monitrc
-chmod 600 /config/monitrc
-
 ### Check monit conf
 test -e /config/monitrc || die "Monit conf not found at: /config/monitrc"
 /usr/local/bin/monit -c /config/monitrc -t || die "Monit conf verification failed"
