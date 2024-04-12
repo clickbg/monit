9e1fd8656ca67d41158958d196ca628aa3921d20
Daniel <daniel@localhost.org>
2024-04-12 11:39:30 +0300

    Allow monit to be run with non-root user #6

--- root/docker-entrypoint.sh
+++ root/docker-entrypoint.sh
@@ -13,10 +13,10 @@ test -e /config/monitrc || die "Monit conf not found at: /config/monitrc"
 ### Add user to passwd since monit fails to start if its user isn't present
 USERID="${PUID:-1000}"
 GROUPID="${GUID:-1000}"
-groupadd -g $GROUPID || die "Failed to add group with GUID:$GROUPID"
-useradd -M -s /sbin/nologin -u $USERID -g $GROUPID -U -d /nonexistent monit || die "Failed to add user monit with PUID:$USERID"
+useradd -M -s /sbin/nologin -u $USERID -U -d /nonexistent monit || die "Failed to add user monit with PUID:$USERID and group with GUID:$GROUPID"
 
 ### Setup logging
+chown monit:monit /run
 chown monit:monit /dev/stdout
 ln -s /dev/stdout /var/log/monit.log
 
@@ -30,4 +30,4 @@ chmod 700 /usr/local/bin/monit
 su monit --shell /bin/bash  -c '/usr/local/bin/monit -c /config/monitrc -t' || die "Monit conf verification failed"
 
 ### Run as non-root user
-su monit --shell /bin/bash  -c '/usr/local/bin/monit -c /config/monitrc -I'
+su monit --shell /bin/bash  -c '/usr/local/bin/monit -c /config/monitrc -p /run/monit.pid -I'
